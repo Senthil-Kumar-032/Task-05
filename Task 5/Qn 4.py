ca6a86f43f69c5a8b9479cb9ab8634a9686f7e8a
Senthil Kumar S <senthilskumar032@gmail.com>
2026-02-13 05:28:10 +0530

    Commit

---
+++ Task 5/Qn 4.py
@@ -0,0 +1,14 @@
+# Write a lambda function to check if a given string is a number.
+
+# Lambda function to check if a string is a number
+is_number = lambda s: s.isdigit()
+
+# Enter the Input
+input_value =input("Enter a number: ")
+
+# Checks the entered value
+if is_number(input_value):
+    print("It is a number")
+else:
+    print("It is not a number")
+
