5b92b581535d5ac24c99059fba3ad685bb490030
Senthil Kumar S <senthilskumar032@gmail.com>
2026-02-13 04:46:56 +0530

    Commit

---
+++ Task 5/Qn 1.py
@@ -0,0 +1,17 @@
+# 1) Given a list of dictionaries, each representing a person with 'name' and 'age' keys,
+# use lambda functions to filter out people under 18 and
+# then map the remaining people's names to a new list.
+
+# List with person name and age
+person = [
+    {"name": "Senthil", "age": 25},
+    {"name": "Siva", "age": 21},
+    {"name": "Raja", "age": 28},
+    {"name": "Jasmine", "age": 16}
+]
+
+# Create a list of names for people age 18 and above
+result = list(map(lambda people: people["name"], filter(lambda people: people["age"] >= 18, person)))
+
+# Displays the result
+print(result)
