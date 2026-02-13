d35232519d3895d2525c9647968df4363e0d7e64
Senthil Kumar S <senthilskumar032@gmail.com>
2026-02-13 06:08:03 +0530

    Commit

---
+++ Task 5/Qn 6.py
@@ -0,0 +1,9 @@
+# Create a lambda function to generate a Fibonacci series up to n terms.
+
+# Import reduce from the functools Package
+from functools import reduce
+
+# # Lambda function to create a Fibonacci series with n terms
+fib_series = lambda n: reduce(lambda a, _: a + [a[-1] + a[-2]], range(n - 2), [0,1] )
+
+print(fib_series(10))
